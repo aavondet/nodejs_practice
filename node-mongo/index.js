25f56776242ead685b485a2bb38971c8e31d8afb
aavondet <arnaud.avondet@plaid.co.jp>
2019-05-22 10:53:48 +0900

    Node-Mongo With Promises
    -Updates code to use promises
    -Avoid "callback hell"

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -5,57 +5,30 @@ const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'test2';
 
-MongoClient.connect(url, (err, client) => {
-    assert.equal(err, null);
-
+MongoClient.connect(url).then(client => {
     console.log('Connected to server');
     const db = client.db(dbname);
-    // const collection = db.collection('users');
     
-    dboper.insertDocument(db, { "name" : "John", "pass" : "Doe"},
-        'users', (result) => {
-            console.log('Inserted document\n', result.ops);
+    dboper.insertDocument(db, { "name" : "John", "pass" : "Doe"},'users')
+    .then((result) => {
+        console.log('Inserted document\n', result.ops);
+        return dboper.findDocuments(db, 'users')
+    })
+    .then((docs) => {
+        console.log('Found documents:\n', docs);
+        return dboper.updateDocument(db, {"name" : "John"}, {"pass":"Moe"}, 'users')
+    })
+    .then((result) => {
+        console.log('Document updated\n', result.result);
+        return dboper.findDocuments(db, 'users')
+    })
+    .then((docs) => {
+        console.log('Found updated document\n', docs);
+        return db.dropCollection('users');
+    })
+    .then((result) => {
+        console.log('Dropped collection\n', result);
 
-            dboper.findDocuments(db, 'users', (docs) => {
-                console.log('Found documents:\n', docs);
-
-                dboper.updateDocument(db, {"name" : "John"}, {"pass":"Moe"}, 'users', (result) => {
-                    console.log('Document updated\n', result.result);
-
-                    dboper.findDocuments(db, 'users', (docs) => {
-                        console.log('Found updated document\n', docs);
-
-                        db.dropCollection('users', (result) => {
-                            console.log('Dropped collection\n', result);
-
-                            client.close();
-                        });
-                    });
-                });
-            });
-
-    });
-    
-    // Implementation wiothout using operations module
-
-    // collection.insertOne({"name": "Bob", "pass": "Ray"}, 
-    //     (err, result) => {
-    //         assert.equal(err, null);
-
-    //         console.log('After Insert:\n');
-    //         console.log(result.ops);
-
-    //         collection.find({}).toArray((err, docs) => {
-    //             assert.equal(err, null);
-
-    //             console.log('Found:\n');
-    //             console.log(docs);
-
-    //             db.dropCollection('users', (err, result) => {
-    //                 assert.equal(err, null);
-
-    //                 client.close();
-    //             });
-    //         });
-    //     });
-});
+        client.close();
+    }).catch((err) => console.log(err));
+}).catch((err) => console.log(err));
